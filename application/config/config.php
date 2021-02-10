748f3c5d54c3e612a90e5cb5efda6561fdfa8cca
kikiAgustin <kikiagustin62@gmail.com>
2021-02-10 18:12:37 +0600

    ubah configurasi

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/aplikasi_penilaian/';
+$config['base_url'] = 'http://149.129.180.250:84/';
 
 /*
 |--------------------------------------------------------------------------
