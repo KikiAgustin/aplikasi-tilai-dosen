c5f06e0f0174403deafc79d4596bc0d7de3c5089
kikiAgustin <kikiagustin62@gmail.com>
2021-02-10 20:56:55 +0600

    cek data

--- application/views/user/login_user.php
+++ application/views/user/login_user.php
@@ -42,7 +42,7 @@
                     </p>
 
                     <p class="isi-sub-login">
-                        Jika Kamu Belum Memiliki Akun
+                        Jika Kamu Belum Memiliki Akun Ya
                         <br>
                         Silahkan <span class="fw-bold"><a class="text-decoration-none" href="<?= base_url('AuthUser/registrasiUser'); ?>">Registrasi</a></span> Disini
                     </p>
